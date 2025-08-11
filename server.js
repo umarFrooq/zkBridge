c7d8ca6416039f152a16ddc64aa46b212f8d2109
umarFarooq <116794065+umarFarooq920@users.noreply.github.com>
2025-08-11 14:41:30 +0500

    up

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const net = require('net');
 const axios = require('axios');
 const url = require('url');
 const querystring = require('querystring');
-
 const TCP_PORT = 4370; // ZKTeco ADMS default
-const API_URL = "https://api-hr-jazeera.codetricksolutions.com/v1/checkin/machine-checkin";
+
+require('dotenv').config()
+const API_URL = process.env.webhook;
+
 
 const server = net.createServer((socket) => {
     const deviceIP = socket.remoteAddress;
@@ -95,3 +97,59 @@ const server = net.createServer((socket) => {
 server.listen(TCP_PORT, () => {
     console.log(`ZKTeco listener running on port ${TCP_PORT}`);
 });
+
+
+
+
+// const net = require('net');
+
+function testZKTecoServer() {
+    const client = new net.Socket();
+    
+    client.connect(4370, 'localhost', () => {
+        console.log('Connected to server');
+        
+        // Simulate ZKTeco attendance data
+        const testData = 
+            "GET /iclock/cdata?SN=12345678&table=ATTLOG HTTP/1.1\r\n" +
+            "Host: localhost:4370\r\n" +
+            "Content-Type: text/plain\r\n" +
+            "Content-Length: 58\r\n" +
+            "\r\n" +
+            "1001\t2024-01-15 09:30:00\t1\t1\r\n" +
+            "1002\t2024-01-15 09:31:00\t0\t1\r\n";
+        
+        client.write(testData);
+        console.log('Sent test attendance data');
+    });
+    
+    client.on('data', (data) => {
+        console.log('Server response:', data.toString());
+        
+        // Test device check-in after receiving response
+        setTimeout(() => {
+            const checkinData = 
+                "GET /iclock/cdata?SN=12345678 HTTP/1.1\r\n" +
+                "Host: localhost:4370\r\n" +
+                "\r\n";
+            
+            client.write(checkinData);
+            console.log('Sent check-in request');
+        }, 1000);
+    });
+    
+    client.on('close', () => {
+        console.log('Connection closed');
+    });
+    
+    client.on('error', (err) => {
+        console.error('Connection error:', err.message);
+    });
+    
+    // Close connection after 5 seconds
+    setTimeout(() => {
+        client.destroy();
+    }, 5000);
+}
+
+// testZKTecoServer();
